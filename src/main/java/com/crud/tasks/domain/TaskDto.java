65b721d7eda5d6c812244abd551e3f2c806505a9
Venomicon <jangajda9@gmail.com>
2019-12-11 19:30:57 +0100

    Modul 29.3

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,13 +1,20 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TaskDto {
+    @JsonProperty(value = "id")
     private Long id;
+
+    @JsonProperty(value = "title")
     private String title;
+
+    @JsonProperty(value = "content")
     private String content;
 }
--- src/test/java/com/crud/tasks/controller/TaskControllerTest.java
+++ src/test/java/com/crud/tasks/controller/TaskControllerTest.java
@@ -1,17 +1,135 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import com.google.gson.Gson;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(MockitoJUnitRunner.class)
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TaskController.class)
 public class TaskControllerTest {
-    @InjectMocks
-    private TaskController taskController;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskMapper taskMapper;
+
+    @MockBean
+    private DbService service;
+
+    private class ReturnAnswer implements Answer {
+        @Override
+        public Task answer(InvocationOnMock invocation) throws Throwable {
+            return new Task(1L, "Test", "Test content");
+        }
+    }
 
     @Test
-    public void testGetTasks() {
+    public void shouldFetchTaskLists() throws Exception {
+        //Given
+        List<TaskDto> taskDtos = new ArrayList<>();
+        taskDtos.add(new TaskDto(1L, "Test", "Test content"));
 
+        when((taskMapper.mapToTaskDtoList(anyList()))).thenReturn(taskDtos);
+
+        //When & Then
+        mockMvc.perform(get("/v1/task/getTasks").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].title", is("Test")))
+                .andExpect(jsonPath("$[0].content", is("Test content")));
     }
+
+    @Test
+    public void shouldFetchTask() throws Exception {
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "Test", "Test content");
+        Task task = new Task(1L, "Test", "Test content");
+
+        when(taskMapper.mapToTaskDto(any(Task.class))).thenReturn(taskDto);
+        when(service.getTask(anyLong())).thenReturn(Optional.of(task));
+
+        //When & Then
+        mockMvc.perform(get("/v1/task/getTask?taskId=1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.title", is("Test")))
+                .andExpect(jsonPath("$.content", is("Test content")));
+    }
+
+    @Test
+    public void shouldDeleteTask() throws Exception {
+        //Given
+        Task task = new Task(1L, "Test", "Test content");
+
+        //When & Then
+        mockMvc.perform(delete("/v1/task/deleteTask?taskId=1").contentType(MediaType.APPLICATION_JSON));
+
+        doAnswer(invocation -> {
+            assertTrue(invocation.getArgument(0).equals(task.getId()));
+            return null;
+        }).when(service).deleteTask(anyLong());
+    }
+
+    @Test
+    public void shouldCreateTask() throws Exception {
+        //Given
+        Task task = new Task(1L, "Test", "Test content");
+        TaskDto taskDto = new TaskDto(1L, "Test", "Test content");
+
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(taskDto);
+
+        when(service.saveTask(any(Task.class))).thenReturn(task);
+
+        //When % Then
+        mockMvc.perform(post("/v1/task/createTask")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(jsonContent));
+    }
+
+    @Test
+    public void shouldUpdateTask() throws Exception {
+        //Given
+        TaskDto taskDto = new TaskDto(1L, "Test", "Test content");
+
+        Gson gson = new Gson();
+        String jsonContent = gson.toJson(taskDto);
+
+        when(taskMapper.mapToTaskDto(any(Task.class))).thenReturn(taskDto);
+
+        //When & Then
+        mockMvc.perform(put("/v1/task/updateTask")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(jsonContent))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.title", is("Test")))
+                .andExpect(jsonPath("$.content", is("Test content")));
+    }
+
 }
